2d0d1117f4fe2c01a90092c40a37998166b64a5d
RitaYang <ritayang0811@gmail.com>
2023-09-05 03:38:08 +0800

    product finish

--- main.js
+++ main.js
@@ -2,26 +2,3 @@ import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 console.log("Hello world!");
-const swiper = new Swiper(".swiper", {
-  // slide 間距
-
-  //   slidesPerView: 1,
-  slidesPerGroup: 4,
-  //斷點
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      slidesPerGroup: 4,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-  },
-  // Scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-  },
-  // 使 swiper 可以使用滑鼠滾輪捲動
-  mousewheel: true,
-});
